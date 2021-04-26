88ae77b2b5cf72a4a4ec5aed18ecab466920eeda
Óscar Pozo Fernández <alu0101036526@ull.edu.es>
2021-04-26 10:39:01 +0100

    Controlado el borrado de fichero

--- src/main.ts
+++ src/main.ts
@@ -22,7 +22,7 @@ const filename = process.argv[2];
 if (!filename) {
   console.log('A file to watch must be specified!');
   // Una posible solución sería crear el fichero y continuar con el programa.
-  fs.writeFile(`${filename}`, ' ', (err) => {
+  fs.writeFile(`${filename}`, 'algo', (err) => {
     if (err) {
       console.log('Ocurrió un error en la creación del fichero.');
       throw err;
@@ -31,10 +31,16 @@ if (!filename) {
   });
 }
 
-
-fs.watch(filename, () => {
+// fs.watch() devuelve un objeto fs.FSWatcher que tiene la función close() para cerrarlo.
+const valor = fs.watch(filename, (eventType, err) => {
   const ls = spawn('ls', ['-l', '-h', filename]);
 
+  // controlamos el tipo de evento que se produce.
+  console.log(`Event Type: ${eventType}`);
+  if (eventType === 'rename') {
+    console.log('El fichero se ha movido/borrado. Terminando ejecución.');
+    valor.close();
+  }
 
   let output = '';
   ls.stdout.on('data', (chunk) => (output += chunk));
@@ -44,4 +50,8 @@ fs.watch(filename, () => {
     const parts = output.split(/​​\s​​+/);
     console.log([parts[0], parts[4], parts[8]]);
   });
+});
+
+valor.on('close', () => {
+  console.log('Terminamos la ejecución.');
 });
